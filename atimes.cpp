28bb62f5385119c4437d14394e50e2cf01132d77
Mathias Rav <rav@cs.au.dk>
2012-04-26 23:38:31 +0200

    Also output t/(n ln n) plot and make plot line more connected

--- atimes.cpp
+++ atimes.cpp
@@ -14,8 +14,10 @@ const int n0 = 1024;
 const double inc = 1.05;
 double measurements[MEASUREMENTS];
 int measured;
+const int downsample_by = 2;
 
-void printstats(int);
+void printstats(std::vector<double> & downsampled);
+void exitstats(int);
 
 int main() {
 	int num = n0;
@@ -25,7 +27,7 @@ int main() {
 	measured = 0;
 	struct sigaction sa;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = printstats;
+	sa.sa_handler = exitstats;
 	if (sigaction(SIGINT, &sa, NULL)) err(1, "sigaction");
 	if (sigaction(SIGHUP, &sa, NULL)) err(1, "sigaction");
 	if (sigaction(SIGQUIT, &sa, NULL)) err(1, "sigaction");
@@ -54,18 +56,42 @@ int main() {
 		num = num * inc;
 		delete[] addresses;
 	}
-	printstats(0);
+	exitstats(0);
 	return 0;
 }
 
-void printstats(int) {
-	const int downsample = 2;
-	const int columns = measured/downsample;
-	double * downsampled = new double[columns];
-	for (int i = 0; i < columns*downsample; ++i) {
+std::vector<double> downsample() {
+	const int columns = measured/downsample_by;
+	std::vector<double> downsampled(columns, 0.0);
+	for (int i = 0; i < columns*downsample_by; ++i) {
 		double m = measurements[i];
-		downsampled[i] += m/downsample;
+		downsampled[i/downsample_by] += m/downsample_by;
 	}
+	return downsampled;
+}
+
+void changestats(std::vector<double> & downsampled) {
+	const int columns = downsampled.size();
+	int n = n0;
+	for (int i = 0; i < columns; ++i) {
+		downsampled[i] = downsampled[i] / log(n);
+		for (int j = 0; j < downsample_by; ++j)
+			n = n * inc;
+	}
+}
+
+void exitstats(int) {
+	printf("\nGraph: Average time per element (log/log)\n");
+	std::vector<double> downsampled = downsample();
+	printstats(downsampled);
+	changestats(downsampled);
+	printf("\nGraph: t/(n ln n) (log/log)\n");
+	printstats(downsampled);
+	exit(0);
+}
+
+void printstats(std::vector<double> & downsampled) {
+	const int columns = downsampled.size();
 	double min = downsampled[0];
 	double max = downsampled[0];
 	for (int i = 0; i < columns; ++i) {
@@ -75,30 +101,41 @@ void printstats(int) {
 		if (m < min)
 			min = m;
 	}
+	min = min / 1.001;
+	max = max * 1.001;
 	const int rows = 40;
 	const int subdivisions = 3;
 	const double step = pow(min/max, 1.0/(rows*subdivisions));
 	double upper = max;
-	printf("\nGraph: Average time per element (log/log)\n");
 	for (int i = 0; i < rows; ++i) {
 		double t1 = upper*step;
 		double t2 = t1*step;
 		double t3 = t2*step;
 		for (int j = 0; j < columns; ++j) {
 			double m = downsampled[j];
+			if (m < t3) {
+				putc(' ', stdout);
+				continue;
+			}
+
+			bool bar = false;
+			if (j > 0 && downsampled[j-1] < t3)
+				bar = true;
+			else if (j < columns-1 && downsampled[j+1] < t3)
+				bar = true;
+
 			if (t1 < m && m < upper) {
-				putc('\'', stdout);
+				putc(bar ? '|' : '\'', stdout);
 			} else if (t2 < m && m < t1) {
 				putc('-', stdout);
 			} else if (t3 < m && m < t2) {
 				putc(',', stdout);
 			} else {
-				putc(' ', stdout);
+				putc(bar ? '|' : ' ', stdout);
 			}
 		}
 		putc('\n', stdout);
 		upper = t3;
 	}
 	fflush(stdout);
-	exit(0);
 }
